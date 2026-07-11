db917838e08ef48cd3ca51645e5f85a60e2316f1
Spider <duriauxbrasilvasco@gmail.com>
2026-07-11 13:09:01 +0200

    variable content handling

--- payment-market.js
+++ payment-market.js
@@ -1,10 +1,6 @@
-const maket = "Payment Maket"
-console.log(maket)
-
+const market = "Payment Market"
+console.log(market)
 
 const picnic = "To prepare the vegetables for the soup, first peel the carrots, chop them into small pieces, grate the fresh ginger, season the broth with salt and pepper, and finally trim away any tough outer edges. She carefully halved each grape, stripped the peel from the apple, chopped the nuts, seasoned the salad, and packed the whole meal into the picnic basket."
-
-
-
 
 const time = "It takes their 15 minutes of daily preparation to master the recipe"
